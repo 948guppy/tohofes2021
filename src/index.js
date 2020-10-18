6cc019e11605668bfb0b90ab225f3c1f16e04e9e
Haru <948guppy@gmail.com>
2020-10-19 00:56:42 +0900

    Arwesのフレームワークを仕様 CHANGE : index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider, createTheme } from 'arwes';
+import { ThemeProvider, createTheme, Arwes, Button } from 'arwes';
 
 const App = () => (
     <ThemeProvider theme={createTheme()}>
-        <div>My Project</div>
+        <Arwes animate show>
+            <div style={{ padding: 20 }}>
+                <h1>My App</h1>
+                <p>A SciFi Project</p>
+                <Button animate show>Travel to Space</Button>
+            </div>
+        </Arwes>
     </ThemeProvider>
 );
 
